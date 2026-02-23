d7ea9a50b47ca0f93493cb7911e02c0539c168a9
ZhouQi-Hust-Math <2517036487@qq.com>
2026-02-24 00:46:38 +0800

    2026-02-24

--- test.py
+++
@@ -1,8 +0,0 @@
-import torch
-from TopoDataSyn import Plot_command
-from TopoDataSyn.NN_model import Netpre
-
-nets = Netpre(width=3, w_in=2, w_out=2, depth=4, acf=torch.nn.ELU())
-print(nets)
-nets.net1 = nets.net1[0: 8]
-print(nets)
